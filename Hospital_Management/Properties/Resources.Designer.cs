82b1f8e0e96d5bac819c4f50b37b9ab9ce085af0
mdshaiyan <mdshaiyanbeanomar@gmail.com>
2026-09-07 03:25:51 +0600

    Background Image Change Of Admin Interface and Increase Button Size

--- Hospital_Management/Properties/Resources.Designer.cs
+++ Hospital_Management/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Hospital_Management.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _574_1024x512 {
+            get {
+                object obj = ResourceManager.GetObject("574-1024x512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cashhier {
             get {
                 object obj = ResourceManager.GetObject("cashhier", resourceCulture);
